85ddbb8dd16cad6c1d1e732c4f93589a4e44d067
zhuyanv587 <1677391824@qq.com>
2019-09-22 19:16:11 +0800

    添加测试test

--- app/src/main/java/com/example/sqlitedemo1/activity/MainActivity.java
+++ app/src/main/java/com/example/sqlitedemo1/activity/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.sqlitedemo1.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.sqlitedemo1.R;
 import com.example.sqlitedemo1.adapter.StudentAdapter;
@@ -28,8 +37,9 @@ public class MainActivity extends AppCompatActivity {
     private List<Student> students;
     private int selectedPos;
     private Student selectedStudent;
-    private Button add, amend, delete;
+    private Button add, amend, delete,read;
     private StudentService studentService;
+    private List<String> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         add = findViewById(R.id.btn_add);
         amend = findViewById(R.id.btn_amend);
         delete = findViewById(R.id.btn_delete);
+        read = findViewById(R.id.btn_read);
         lv_list = findViewById(R.id.lv_list);
 
         studentAdapter = new StudentAdapter(students);
@@ -62,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
                 selectedPos = position;
                 selectedStudent = (Student) parent.getItemAtPosition(position);
-
+                Toast.makeText(MainActivity.this,selectedStudent.toString(),Toast.LENGTH_LONG).show();
                 amend.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -84,9 +95,57 @@ public class MainActivity extends AppCompatActivity {
                         studentAdapter.notifyDataSetChanged();
                     }
                 });
+                read.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //判断是否有运行时权限
+                        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                                Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.READ_CONTACTS},1);
+                        }else {
+                            //读取联系人
+                            readContacts();//需要申请运行权限
+                        }
+                    }
+                });
             }
         });
 
+    }
+
+    private void readContacts() {
+        //获取联系人的cursor，组装联系人字符串放入list
+        Cursor cursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,null,null,null);
+        contacts = new ArrayList<>();
+        if (cursor != null && cursor.moveToFirst()){
+            do{
+                String name = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phone = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contacts.add(name + "："+ phone);
+            }while (cursor.moveToNext());
+            cursor.close();
+        }
+        //设置Adapter
+        if (contacts.isEmpty()){
+            Toast.makeText(MainActivity.this,"没有联系人",Toast.LENGTH_LONG).show();
+            return;
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,contacts);
+        lv_list.setAdapter(arrayAdapter);
+    }
+
+    //运行时权限申请的回调
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            readContacts();
+        }
     }
 
     // 从SQLite数据库获取数据
